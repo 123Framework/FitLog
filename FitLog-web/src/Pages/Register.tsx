6d7a618d1aa250e40c5391c67890405a0b788ee9
123Framework <balatozar@gmail.com>
2025-10-30 17:55:14 +0300

    Add files via upload

---
+++ FitLog-web/src/Pages/Register.tsx
@@ -0,0 +1,6 @@
+import React, { useState } from 'react'
+import { api } from '../api'
+export default function Register() {
+    const [form, setForm] = useState({ username: '', password: '' })
+    const [msg, setMsg] = useState('')
+}

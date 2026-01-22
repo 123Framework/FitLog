a8e9b2a158d419fb6bf74e29eb0434fa498237a4
123Framework <balatozar@gmail.com>
2026-01-22 17:09:58 +0300

    Add files via upload

---
+++ Models/ViewModels/UserGoalDto.cs
@@ -0,0 +1,15 @@
+﻿namespace FitLog.Models.ViewModels
+{
+    public class UserGoalDto
+    {
+        public double? TargetWeight { get; set; }
+        public DateTime? TargetDate { get; set; }
+
+        public int? DailyCalories { get; set; }
+        public int? DailyProtein { get; set; }
+        public int? DailyFat { get; set; }
+        public int? DailyCarbs { get; set; }
+
+
+    }
+}

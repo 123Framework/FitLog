554682cf83f58677ceae5fc53bfb1b88e139b539
123Framework <balatozar@gmail.com>
2026-02-12 17:24:19 +0300

    Add files via upload

---
+++ Models/AiRequest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FitLog.Models
+{
+    public class AiRequest { 
+    public string Model { get; set; }
+        public List<MessageDto> Messages { get; set; }
+    
+    }
+        
+    public class MessageDto
+    {
+        public string Role { get; set; }
+        public string Content { get; set; }
+
+
+    }
+
+
+
+}
---
+++ Models/AiResponse.cs
@@ -0,0 +1,22 @@
+﻿namespace FitLog.Models
+{
+    public class AiResponse
+    {
+
+        public List<AIChoice> Choices { get; set; }
+
+
+    }
+    public class AIChoice
+    {
+        public AIMessage Message { get; set; }
+    }
+    public class AIMessage { 
+    
+    
+    public string Role{ get; set; }
+    public string Content{ get; set; }
+
+    
+    }
+}

a7c500c47e846b36b4c3c4fa58b66bcd92e7e424
123Framework <balatozar@gmail.com>
2026-02-12 17:39:38 +0300

    Add files via upload

--- Models/AiResponse.cs
+++ Models/AiResponse.cs
@@ -1,22 +1,27 @@
-﻿namespace FitLog.Models
+﻿using System.Text.Json.Serialization;
+
+namespace FitLog.Models
 {
     public class AiResponse
     {
-
+        [JsonPropertyName("choices")]
         public List<AIChoice> Choices { get; set; }
 
 
     }
     public class AIChoice
     {
+        [JsonPropertyName("message")]
         public AIMessage Message { get; set; }
     }
-    public class AIMessage { 
-    
-    
-    public string Role{ get; set; }
-    public string Content{ get; set; }
+    public class AIMessage
+    {
 
-    
+        [JsonPropertyName("role")]
+        public string Role { get; set; }
+        [JsonPropertyName("content")]
+        public string Content { get; set; }
+
+
     }
 }
